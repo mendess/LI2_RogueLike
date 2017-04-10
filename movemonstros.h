0701d68f992f9e13d42473b43cd12394ab5bf736
Mendess2526 <pedro.mendes.26@gmail.com>
2017-04-10 01:25:58 +0100

    Created movemonstros.h
    Não te esqueças de adicionar #include "movemonstros.h" no movemonstros.c

---
+++ movemonstros.h
@@ -0,0 +1,21 @@
+#ifndef __MVMON_H__
+#define __MVMON_H__
+
+#include "jogo.h"
+
+#define WOLF_DMG      10
+#define WOLF_HP       10
+#define WOLF_SCORE    10
+#define BAT_DMG       7
+#define BAT_HP        10
+#define BAT_SCORE     10
+#define ARCHER_DMG    8
+#define ARCHER_HP     8
+#define ARCHER_SCORE  8
+#define OGRE_DMG      15
+#define OGRE_HP       15
+#define OGRE_SCORE    15
+
+ESTADO movemonstros(ESTADO e);
+
+#endif
