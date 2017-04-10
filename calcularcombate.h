80294ecbe5d1562dbbce55a1b0e6c9fb541c895c
Mendess2526 <pedro.mendes.26@gmail.com>
2017-04-10 01:46:08 +0100

    Created calcularcombate.h
    Não te esqueças de adicionar #include "calcularcombate.h" no calcularcombate.c

---
+++ calcularcombate.h
@@ -0,0 +1,13 @@
+#ifndef __CLCOMB_H__
+#define __CLCOMB_H__
+
+#include "jogo.h"
+
+#define WOLF_SCORE    10
+#define BAT_SCORE     10
+#define ARCHER_SCORE  8
+#define OGRE_SCORE    15
+
+ESTADO calcularCombate(e);
+
+#endif
