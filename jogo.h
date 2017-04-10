a219b5e0e1fd650c02c81a06ac43317d7e6366fd
Mendess2526 <pedro.mendes.26@gmail.com>
2017-04-10 01:18:15 +0100

    Fiz update ao estado e adicionei outras structs

--- jogo.h
+++ jogo.h
@@ -2,16 +2,64 @@
 #define ___JOGO_H___
 
 #define TAM           50
-#define MAX_MONSTROS  10
-#define MAX_PEDRAS    20
+#define MAX_MONSTROS  5
+#define MAX_PEDRAS    25
 #define SIZE          10
+
+#define HP_WARRIOR    100
+#define HP_ARCHER     90
+#define HP_MAGE       70
+
+#define MP_WARRIOR    50
+#define MP_ARCHER     70
+#define MP_MAGE     100
+
+/*
+HelhPotion      0
+ManaPotion      1
+ScrollFire      2  - Vermelho
+ScrollLightning 3  - Amarelo
+ScrollTeleport  4  - Azul
+SwordBronze     5  - Castanho
+SwordIron       6  - Cinzento Escuro
+SwordSteel      7  - Cinzento Claro
+SwordPalladium  8  - Inventa para ai
+ArmourBronze    9  - Castanho
+ArmourIron      10 - Cinzento Escuro
+ArmourSteel     11 - Cinzento Claro
+ArmourPalladium 12 - Inventa para ai
+*/
 
 typedef struct posicao{
 	char x;
 	char y;
 }POSICAO;
 
-typedef struct estado {
+typedef struct monster{
+    char x;
+    char y;
+    char monType;
+    char hp;
+}MSTR;
+
+typedef struct inventory{
+    char gold;
+    char inv[4];
+}INVT;
+
+typedef struct estado{
+    //Classe {Warrior=0, Archer=1, Mage=2}
+    char classe;
+    //Vida do jogador
+    char hp;
+    //Mana do jogador
+    char mp;
+    //Nivel
+    char world_lvl;
+    //Score
+    int score;
+    //Turno
+    char turn;
     //Lado para que o jogador esta a olhar 0:direita e 1:esquerda
     char direction;
     //Action
@@ -40,6 +88,6 @@ ESTADO inicializar();
 int com_jogador (ESTADO e,POSICAO p);
 int com_pedras (ESTADO e, POSICAO p);
 int com_monstros (ESTADO e, POSICAO p);
-ESTADO ler_estado (char *args);
+ESTADO ler_estado (char *args,FILE *e);
 
 #endif
