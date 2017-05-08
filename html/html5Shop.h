c1654e83a9d536dd83de2f67e69e64ed61d017a9
Pedro Mendes <pedro.mendes.26@gmail.com>
2017-05-08 19:56:21 +0100

    completed html5Shop.h

--- html/html5Shop.h
+++ html/html5Shop.h
@@ -7,6 +7,9 @@
 void imprime_shop_item(char item, int index);
 void imprime_inv_slot_shop(char item,int i);
 void imprime_equipment_shop(INVT bag);
+void imprime_price(int x, int y, int amount);
+void imprime_shop_prices(char lootTable[],int gold);
+void imprime_shop_messages(char shopFeedback);
 void imprimeShop(ESTADO e);
 
 #endif
