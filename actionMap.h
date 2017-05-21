38eeded5b4f23ade7fa2160a32548efadcefbec3
Pedro Mendes <pedro.mendes.26@gmail.com>
2017-05-21 12:26:42 +0100

    actionMap finished, files that use it altered acordingly

---
+++ actionMap.h
@@ -0,0 +1,18 @@
+#ifndef __ACTION_MAP__
+#define __ACTION_MAP__
+
+#define ACT_GOTO_MENU		e.action==0
+#define ACT_MENU_CHOICE		e.action>50 && e.action<60
+#define ACT_CLASS_CHOICE	e.action>60 && e.action<70
+#define ACT_MOVE			e.action>0 && e.action<10
+#define ACT_ATACK			e.action>10 && e.action<20
+#define ACT_EXIT			e.action==5
+#define ACT_SHOP_CHOICE		e.action>69 && e.action<82
+#define PLR_FACING_LEFT		e.action==9 || e.action==6 || e.action==3
+#define PLR_FACING_RIGHT	e.action==7 || e.action==4 || e.action==1
+#define ACT_USE_ITEM		e.action>39 && e.action<50
+#define ACT_RANGED_ATTACK	e.action>20 && e.action<30
+#define ACT_BOSS_ATTACK		e.action==30
+
+
+#endif
