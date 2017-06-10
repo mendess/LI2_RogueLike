19a4ac752426c0be8cba5e2f03ac79e279efe318
Pedro Mendes <pedro.mendes.26@gmail.com>
2017-06-10 19:18:03 +0100

    restored IAdragon.h

---
+++ IA/IAdragon.h
@@ -0,0 +1,8 @@
+#include "IA.h"
+
+ESTADO fireCone(ESTADO e);
+ESTADO wing_attack (ESTADO e);
+ESTADO fireball_damage (ESTADO e);
+ESTADO fireball_attack (ESTADO e);
+ESTADO dragon_movement (ESTADO e);
+ESTADO estrat_dragon (ESTADO e);
